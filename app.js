46a48c3a20f592662b4497efe53f9479dad9e4ed
nikitasmall <nikitasosnov@yahoo.com>
2015-06-19 14:20:02 +0300

    upload implemented, mongoDB model

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,10 +16,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// set folder for photos
+app.set('photos', path.join(__dirname, 'public/photos'));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(multer({ dest: app.get('photos') }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/photos.js
+++ routes/photos.js
@@ -1,23 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-var photos = [];
+var Photo = require('../models/photo');
+var path = require('path');
+var fs = require('fs');
+var join = path.join;
 
-photos.push({
-  name: 'Node logo',
-  path: 'http://nodejs.org/images/logos/nodejs-green.png'
-});
-
-photos.push({
-  name: 'Ryan Speaking',
-  path: 'http://nodejs.org/images/ryan-speaker.jpg'
-});
-
+// get photo index page (show all photos)
 router.get('/', function(req, res, next) {
-  res.render('photos/index', {
-    title: 'Stock photos exchange',
-    photos: photos
-    });
+  Photo.find({}, function(err, photos) {
+    if (err) return next(err);
+    res.render('photos/index', {
+      title: 'Stock photos exchange',
+      photos: photos
+      });
+  });
+});
+
+router.get('/upload', function(req, res, next) {
+  res.render('photos/upload', { title: 'Upload your favorite picture' });
+});
+
+router.post('/upload', function(req, res, next) {
+  var photo = req.files.photo;
+  var path = join('/photos', photo.name);
+  var name = req.body.photo.name || photo.name;
+
+  Photo.create({
+    name: name,
+    path: path
+  }, function(err) {
+    if (err) return next(err);
+    res.redirect('/photos');
+  });
 });
 
 module.exports = router;
