1432744a9a5ce43c3b5e5784d8f157cdcf2a1d64
nikitasmall <nikitasosnov@yahoo.com>
2015-06-19 11:20:13 +0300

    new controller, foundation support implemented

---
+++ routes/photos.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var photos = [];
+
+photos.push({
+  name: 'Node logo',
+  path: 'http://nodejs.org/images/logos/nodejs-green.png'
+});
+
+photos.push({
+  name: 'Ryan Speaking',
+  path: 'http://nodejs.org/images/ryan-speaker.jpg'
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Stock photos exchange',
+    photos: photos
+    });
+});
+
+module.exports = router;
